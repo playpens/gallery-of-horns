07e0a59def3b9a1f1cdf8c8abafe2efe2652dfa4
John Cokos <highlander.44@gmail.com>
2023-11-28 09:46:46 -0800

    adds css and notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
+import Header from './components/Header.jsx';
+
+// brings in a CSS file, raw
+// this is the same as <link rel="stylesheet" ... />
+import './index.css'
+
+// This is a CSS "module" which turns the CSS
+// into an object. You must name it!
+import aBunchOfStuff from './john.module.css';
+
+
+// "inline" object that kinda looks like CSS
+const styles = {
+  purple: {
+    backgroundColor: 'rebeccapurple'
+  }
+};
+
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      <div style={styles.purple}>
+        This is a bunch of stuff
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Words from '../Words.jsx';
+
+function Header() {
+    return <Words />
+}
+
+export default Header;
